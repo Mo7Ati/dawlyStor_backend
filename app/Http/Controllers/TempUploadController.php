4a7c30cb981db125368f097329c0feed6abce97d
Mohammed Abd Al Ati <Mo7.dawly@gmail.com>
2026-03-24 03:01:07 +0200

    Update TempUploadController to use Storage facade for improved file management.

--- app/Http/Controllers/TempUploadController.php
+++ app/Http/Controllers/TempUploadController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Inertia\Inertia;
+use Illuminate\Support\Facades\Storage;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class TempUploadController extends Controller
